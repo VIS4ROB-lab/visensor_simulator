323dd6734a2b3929eb6aa404a41460771f272d3a
lucas <weblucas@gmail.com>
2018-06-13 10:18:42 +0200

    bagcreator lite- imu been inserted. Images missing

--- scripts/bagcreator_lite.py
+++ scripts/bagcreator_lite.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #License (BSD)
 #Copyright (c) 2014, Paul Furgale, Jérôme Maye and Jörn Rehder, Autonomous Systems Lab, ETH Zurich, Switzerland
 #Copyright (c) 2014, Thomas Schneider, Skybotix AG, Switzerland
@@ -15,7 +16,87 @@ import argparse
 import cv2
 import numpy as np
 import csv
+import json
 
+
+## Dataformat for the JSON project
+
+class VISimCamera():
+    cam_name = "cam_default"
+    focal_lenght = 455
+    frequency_reduction_factor = 99 # if 10 then it is 10 times SLOWER than the imu that runs at 200Hz
+    height = 480
+    width = 752
+    transform_ImuCamera = [0, -1, 0, 0, #frontal camera in ros standard
+                          -1, 0, 0, 0,
+                          0, 0, -1, 0,
+                          0, 0, 0, 1] 
+    #todo add param with the delay between the imu and image
+    
+    
+    
+    def toJSON(self):
+        result = {}
+        result["cam_name"] = self.cam_name
+        result["focal_lenght"] = self.focal_lenght
+        result["frequency_reduction_factor"] = self.frequency_reduction_factor
+        result["height"] = self.height
+        result["width"] = self.width
+        result["T_SC"] = self.transform_ImuCamera
+        return result
+    
+    def fromJSON(self, json_dict):
+        try:
+            self.cam_name = json_dict["cam_name"]
+            self.focal_lenght = json_dict["focal_lenght"]
+            self.frequency_reduction_factor = json_dict["frequency_reduction_factor"]
+            self.height = json_dict["height"]
+            self.width = json_dict["width"]
+            self.transform_ImuCamera = json_dict["T_SC"]
+        except KeyError as e:
+            print ('KeyError - cam_id {} reason {}'.format(self.cam_name , str(e)))
+            return False
+        return True
+
+class VISimProject():
+    
+    cameras=[]
+    name="" 
+    
+    def camerasToJSON(self):
+        result = []
+        for cam in self.cameras:
+            result.append(cam.toJSON())
+        return result
+        
+    def camerasFromJSON(self, cameraJSONs):
+        result = []
+        for iCamJSON in cameraJSONs:
+            currCam = VISimCamera()
+            if(currCam.fromJSON(iCamJSON)):            
+                result.append(currCam)
+            else:
+                return False
+                
+        self.cameras = result
+        
+        return True
+    
+    def toJSON(self):
+        result = {}
+        result["name"] = self.name
+        result["cameras"] = self.camerasToJSON()
+        return result
+    
+    def fromJSON(self, json_dict):
+        try:
+            self.name = json_dict["name"]
+        except KeyError as e:
+            print ("KeyError - in project reason {}".format(str(e)))
+            
+        if(self.camerasFromJSON(json_dict["cameras"]) == False):
+            return False
+        return True
 
 
 
@@ -27,11 +108,11 @@ def getImageFilesFromDir(dir):
         for path, names, files in os.walk(dir):
             for f in files:
                 if os.path.splitext(f)[1] in ['.bmp', '.png', '.jpg']:
-                    image_files.append( os.path.join( path, f ) )
-                    timestamps.append(int(os.path.splitext(f)[0])) 
+                    image_files.append( f ) 
+                    
     #sort by timestamp
-    sort_list = sorted(zip(timestamps, image_files))
-    image_files = [file[1] for file in sort_list]
+    image_files = sorted( image_files)
+   # image_files = [file for file in sort_list]
     return image_files
 
 def getCamFoldersFromDir(dir):
@@ -44,19 +125,10 @@ def getCamFoldersFromDir(dir):
                     cam_folders.append((folder[-4:],folder))
     return cam_folders
 
-def getImuCsvFiles(dir):
-    '''Generates a list of all csv files that start with imu'''
-    imu_files = list()
-    if os.path.exists(dir):
-        for path, folders, files in os.walk(dir):
-            for file in files:
-                if file[0:3] == 'imu' and os.path.splitext(file)[1] == ".csv":
-                    imu_files.append( os.path.join( path, file ) )
-    
-    return imu_files
 
-def loadImageToRosMsg(filename):
-    image_np = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+def loadImageToRosMsg(camdir,filename):
+    filepath = os.path.join(camdir, filename)
+    image_np = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     
     timestamp_nsecs = os.path.splitext(os.path.basename(filename))[0]
     timestamp = rospy.Time( secs=int(timestamp_nsecs[0:-9]), nsecs=int(timestamp_nsecs[-9:]) )
@@ -86,6 +158,29 @@ def createImuMessge(timestamp_int, omega, alpha):
     
     return rosimu, timestamp
     
+def lixo():
+    #write imu data 
+    imufiles = getImuCsvFiles(imu_filepath)
+    for imufile in imufiles:
+        topic = os.path.splitext(os.path.basename(imufile))[0]
+        with open(imufile, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            headers = next(reader, None)
+            for row in reader:
+                imumsg, timestamp = createImuMessge(row[0], row[1:4], row[4:7])
+                bag.write("/{0}".format(topic), imumsg, timestamp)
+                
+                 #write images
+    camfolders = getCamFoldersFromDir(parsed.folder)
+    for camfolder in camfolders:
+        camdir = parsed.folder + "/{0}".format(camfolder)
+        image_files = getImageFilesFromDir(camdir)
+        for image_filename in image_files:
+            image_msg, timestamp = loadImageToRosMsg(image_filename)
+            bag.write("/{0}/image_raw".format(camfolder), image_msg, timestamp)
+
+    
+    
 
 
 if __name__ == "__main__":
@@ -102,30 +197,45 @@ if __name__ == "__main__":
 
     #parse the args
     parsed = parser.parse_args()
-
+    proj_filepath = os.path.join(parsed.folder, 'visim_project.json')
+        
     #create the bag
     try:
+        with open(proj_filepath) as json_data:
+            try:
+                project_data = json.load(json_data)
+            except ValueError as e:
+                print('Problem with the JSON file parsing. '+ str(e))                
+                sys.exit()
+                
         bag = rosbag.Bag(parsed.output_bag, 'w')
-        filepath = os.path.join(project_object.visim_project_setting.project_folder, 'visim_project.json')
-        #write images
-        camfolders = getCamFoldersFromDir(parsed.folder)
-        for camfolder in camfolders:
-            camdir = parsed.folder + "/{0}".format(camfolder)
-            image_files = getImageFilesFromDir(camdir)
-            for image_filename in image_files:
-                image_msg, timestamp = loadImageToRosMsg(image_filename)
-                bag.write("/{0}/image_raw".format(camfolder), image_msg, timestamp)
-
-        #write imu data
-        imufiles = getImuCsvFiles(parsed.folder)
-        for imufile in imufiles:
-            topic = os.path.splitext(os.path.basename(imufile))[0]
-            with open(imufile, 'rb') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                headers = next(reader, None)
-                for row in reader:
-                    imumsg, timestamp = createImuMessge(row[0], row[1:4], row[4:7])
-                    bag.write("/{0}".format(topic), imumsg, timestamp)
+       
+        visim_json_project = VISimProject()
+        if (visim_json_project.fromJSON(project_data) == False):            
+             print( 'Problem with the JSON file parsing. Look in the terminal')
+             sys.exit()
+        
+        imu_filepath = os.path.join(parsed.folder, 'output/1_Rotors/imu_data.csv')
+        
+        print("okkkk " + visim_json_project.name)
+        imu_topic = "/" + visim_json_project.name + "/imu"
+        with open(imu_filepath, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            headers = next(reader, None)
+            for row in reader:
+                imumsg, timestamp = createImuMessge(row[0], row[1:4], row[4:7])
+                bag.write("/{0}".format(imu_topic), imumsg, timestamp)
+       
+        for cam_data in visim_json_project.cameras:
+            cam_dirpath = os.path.join(parsed.folder, 'output/2_Blender/' + cam_data.cam_name + '_rgbd')
+            cam_topic = "/" + visim_json_project.name + "/" + cam_data.cam_name
+            cam_image_files = getImageFilesFromDir(cam_dirpath)            
+            image_ids = [int(file[3:-4]) for file in cam_image_files]
+            print(image_ids)
+           # for image_filename in image_files:
+           #     image_msg, timestamp = loadImageToRosMsg(cam_dirpath,image_filename)
+                
+        
     finally:
         bag.close()
     
