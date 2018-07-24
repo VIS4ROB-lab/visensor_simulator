fe742eef1365bba9d0d1a967ff46277ee88dcdda
lucas <weblucas@gmail.com>
2018-07-24 15:58:28 +0200

    add gimbal in the zigzag generator and small style changes.

--- scripts/waypoints_zigzag_generator.py
+++ scripts/waypoints_zigzag_generator.py
@@ -9,18 +9,19 @@ def main():
     #setup the argument list
     parser = argparse.ArgumentParser(description='Generate a zig-zag trajectory (ex. -o testfile.txt  ')
     parser.add_argument('-o','--output',  metavar='filename', help='output waypoint file')
-    parser.add_argument('-bbn','--bound_box_north',  metavar='value', type=float,default=15,  help='max lenght of the trajectory in the north direction (in meters)')
-    parser.add_argument('-bbe','--bound_box_east',  metavar='value', type=float,default=6,  help='max lenght of the trajectory in the east direction (in meters)')
+    parser.add_argument('-bbn','--bound_box_north',  metavar='value', type=float,default=80,  help='max lenght of the trajectory in the north direction (in meters)')
+    parser.add_argument('-bbe','--bound_box_east',  metavar='value', type=float,default=80,  help='max lenght of the trajectory in the east direction (in meters)')
     parser.add_argument('-bbu','--bound_box_up',  metavar='value', type=float,default=0, help='max lenght of the trajectory in the up direction (in meters)')
     
-    parser.add_argument('-sn','--step_north',  metavar='value', type=float,default=1,  help='step size in the north direction (in meters)')
-    parser.add_argument('-se','--step_east',  metavar='value', type=float,default=1,  help='step size in the east direction (in meters)')
+    parser.add_argument('-sn','--step_north',  metavar='value', type=float,default=10,  help='step size in the north direction (in meters)')
+    parser.add_argument('-se','--step_east',  metavar='value', type=float,default=80,  help='step size in the east direction (in meters)')
     parser.add_argument('-su','--step_up',  metavar='value', type=float,default=0, help='step size in the up direction (in meters)')
     
-    parser.add_argument('-on','--offset_north',  metavar='value', type=float,default=1, help='offset on north direction (in meters)')
-    parser.add_argument('-oe','--offset_east',  metavar='value', type=float,default=3, help='offset on east direction (in meters)')
-    parser.add_argument('-ou','--offset_up',  metavar='value', type=float,default=1, help='offset on up direction (in meters). Simulators usually cannot work with up= 0')
-    parser.add_argument('-he','--heading',  metavar='value', type=float,default=180, help='desired constant heading-yaw (degrees)')
+    parser.add_argument('-on','--offset_north',  metavar='value', type=float,default=-40, help='offset on north direction (in meters)')
+    parser.add_argument('-oe','--offset_east',  metavar='value', type=float,default=40, help='offset on east direction (in meters)')
+    parser.add_argument('-ou','--offset_up',  metavar='value', type=float,default=20, help='offset on up direction (in meters). Simulators usually cannot work with up= 0')
+    parser.add_argument('-he','--heading',  metavar='value', type=float,default=0, help='desired constant heading-yaw (degrees)')
+    parser.add_argument('-gp','--gimbal_pitch',  metavar='value', type=float,default=90, help='desired constant gimbal pitch (degrees)')
     
 
 #todo Add validation to the input    
@@ -60,7 +61,7 @@ def main():
             for col in range(num_rows_east):
                 e =  min(col*parsed.step_east,parsed.bound_box_east)
                 e_directional = e if going_east else parsed.bound_box_east-e
-                output_file.write('{0:.2f},{1:.2f},{2:.2f},{3:.2f}\n'.format( n,-e_directional+parsed.offset_east,u,parsed.heading))
+                output_file.write('{0:.2f},{1:.2f},{2:.2f},{3:.2f},{4:.2f}\n'.format( n,-e_directional+parsed.offset_east,u,parsed.heading,parsed.gimbal_pitch))
             going_east = not going_east
     
     print "done!"            
